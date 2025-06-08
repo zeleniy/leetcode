0614662c5bfe232eeb630378199891980a5f107e
Aleksandr S. Zelenin <aleksandr.s.zelenin@gmail.com>
2025-06-08 13:49:56 +0300

    feat(problem): solve "Reverse String" recursive with Go

--- reverse-string/solution.go
+++ reverse-string/solution.go
@@ -1,6 +1,21 @@
 package reverse_string
 
-func reverseString(s []byte) {
+func reverseStringRecursive(s []byte) {
+
+	n := len(s)
+
+	if n <= 1 {
+		return
+	}
+
+	tmp := s[0]
+	s[0] = s[n-1]
+	s[n-1] = tmp
+
+	reverseStringRecursive(s[1 : n-1])
+}
+
+func reverseStringIterative(s []byte) {
 
 	for i, n := 0, len(s); i < n/2; i++ {
 		tmp := s[i]
