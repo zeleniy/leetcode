846ab5e549c2dfb3900af0693e2f6c8ba92a7f16
Aleksandr S. Zelenin <aleksandr.s.zelenin@gmail.com>
2025-06-12 18:50:19 +0300

    feat(problem): solve "Number of Islands" by DFS with Go

--- number-of-islands/solution.go
+++ number-of-islands/solution.go
@@ -1,8 +1,8 @@
 package number_of_islands
 
-var neighbors = [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+var neighbors = [4][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
-func numIslands(grid [][]byte) int {
+func numIslandsDfs(grid [][]byte) int {
 
 	m := len(grid)
 	n := len(grid[0])
@@ -12,8 +12,7 @@ func numIslands(grid [][]byte) int {
 		for j := 0; j < n; j++ {
 			if grid[i][j] == '1' {
 				counter++
-				grid[i][j] = '0'
-				markIsland(grid, m, n, i, j)
+				markIslandDfs(grid, m, n, i, j)
 			}
 		}
 	}
@@ -21,9 +20,52 @@ func numIslands(grid [][]byte) int {
 	return counter
 }
 
-func markIsland(grid [][]byte, m, n, i, j int) {
+func markIslandDfs(grid [][]byte, m, n, i, j int) {
 
-	queue := [][]int{{i, j}}
+	stack := make([][2]int, 1, 8)
+	stack[0] = [2]int{i, j}
+	grid[i][j] = '0'
+
+	for len(stack) > 0 {
+
+		stackLength := len(stack)
+
+		i, j := stack[stackLength-1][0], stack[stackLength-1][1]
+		stack = stack[:stackLength-1]
+
+		for _, neighbor := range neighbors {
+			y, x := i-neighbor[0], j-neighbor[1]
+			if y >= 0 && y < m && x >= 0 && x < n && grid[y][x] == '1' {
+				grid[y][x] = '0'
+				stack = append(stack, [2]int{y, x})
+			}
+		}
+	}
+}
+
+func numIslandsBfs(grid [][]byte) int {
+
+	m := len(grid)
+	n := len(grid[0])
+	counter := 0
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == '1' {
+				counter++
+				markIslandBfs(grid, m, n, i, j)
+			}
+		}
+	}
+
+	return counter
+}
+
+func markIslandBfs(grid [][]byte, m, n, i, j int) {
+
+	queue := make([][2]int, 1, 8)
+	queue[0] = [2]int{i, j}
+	grid[i][j] = '0'
 
 	for len(queue) > 0 {
 
@@ -31,11 +73,9 @@ func markIsland(grid [][]byte, m, n, i, j int) {
 
 		for _, neighbor := range neighbors {
 			y, x := i-neighbor[0], j-neighbor[1]
-			if y >= 0 && y < m && x >= 0 && x < n {
-				if grid[y][x] == '1' {
-					grid[y][x] = '0'
-					queue = append(queue, []int{y, x})
-				}
+			if y >= 0 && y < m && x >= 0 && x < n && grid[y][x] == '1' {
+				grid[y][x] = '0'
+				queue = append(queue, [2]int{y, x})
 			}
 		}
 
