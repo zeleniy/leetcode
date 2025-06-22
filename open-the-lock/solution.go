734db34423cbd45055300394e741d5713a21c23d
Aleksandr Zelenin <aleksandr.s.zelenin@gmail.com>
2025-06-22 22:49:36 +0300

    perf: improve "Open the Lock" with Go

--- open-the-lock/solution.go
+++ open-the-lock/solution.go
@@ -1,19 +1,25 @@
 package open_the_lock
 
+var neighbors [8]int
+var forward = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+var backward = [10]int{9, 0, 1, 2, 3, 4, 5, 6, 7, 8}
+
 func openLock(deadends []string, target string) int {
 
-	visited := make(map[string]bool)
+	targetInt := toInt(target)
+	var visited [10000]bool
 
 	for _, deadend := range deadends {
-		visited[deadend] = true
+		visited[toInt(deadend)] = true
 	}
 
-	if visited["0000"] {
+	if visited[0] {
 		return -1
 	}
 
-	queue := []string{"0000"}
-	visited["0000"] = true
+	queue := make([]int, 1, 8192)
+	queue[0] = 0
+	visited[0] = true
 
 	for moves := 0; len(queue) > 0; moves++ {
 		for levelSize := len(queue); 0 < levelSize; levelSize-- {
@@ -21,12 +27,13 @@ func openLock(deadends []string, target string) int {
 			current := queue[0]
 			queue = queue[1:]
 
-			if current == target {
+			if current == targetInt {
 				return moves
 			}
 
-			for _, newNumber := range getNeighbors(current) {
-				if _, exists := visited[newNumber]; !exists {
+			populateCombinations(current)
+			for i := 0; i < 8; i++ {
+				if newNumber := neighbors[i]; !visited[newNumber] {
 					visited[newNumber] = true
 					queue = append(queue, newNumber)
 				}
@@ -37,25 +44,24 @@ func openLock(deadends []string, target string) int {
 	return -1
 }
 
-func getNeighbors(number string) [8]string {
+func populateCombinations(num int) {
 
-	neighbors := [8]string{}
+	d0 := num % 10
+	d1 := (num / 10) % 10
+	d2 := (num / 100) % 10
+	d3 := num / 1000
 
-	for i := 0; i < 4; i++ {
-
-		digit := int(number[i] - '0')
-
-		neighbors[i*2] = modifyDigit(number, digit, i, 1)
-		neighbors[i*2+1] = modifyDigit(number, digit, i, -1)
-	}
-
-	return neighbors
+	neighbors[0] = num - d0 + forward[d0]
+	neighbors[1] = num - d0 + backward[d0]
+	neighbors[2] = num - d1*10 + forward[d1]*10
+	neighbors[3] = num - d1*10 + backward[d1]*10
+	neighbors[4] = num - d2*100 + forward[d2]*100
+	neighbors[5] = num - d2*100 + backward[d2]*100
+	neighbors[6] = num - d3*1000 + forward[d3]*1000
+	neighbors[7] = num - d3*1000 + backward[d3]*1000
 }
 
-func modifyDigit(number string, digit, i, delta int) string {
+func toInt(s string) int {
 
-	bytes := []byte(number)
-	bytes[i] = byte((digit+delta+10)%10 + '0')
-
-	return string(bytes)
+	return int(s[0]-'0')*1000 + int(s[1]-'0')*100 + int(s[2]-'0')*10 + int(s[3]-'0')
 }
