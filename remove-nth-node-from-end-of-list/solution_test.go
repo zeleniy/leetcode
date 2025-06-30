27346e09aaabc135d837bb61f14159913c988816
Aleksandr S. Zelenin <aleksandr.s.zelenin@gmail.com>
2025-06-30 14:29:06 +0300

    chore: add README.md file

--- remove-nth-node-from-end-of-list/solution_test.go
+++ remove-nth-node-from-end-of-list/solution_test.go
@@ -8,17 +8,26 @@ import (
 
 type TestCase struct {
 	answer []int
-	target int
+	n      int
 	list   *ListNode
+}
+
+func BenchmarkRemoveNthFromEnd(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		for _, data := range getTestDataSet() {
+			removeNthFromEnd(data.list, data.n)
+		}
+	}
 }
 
 func TestRemoveNthFromEnd(t *testing.T) {
 
-	for _, dataSet := range getTestDataSet() {
+	for _, data := range getTestDataSet() {
 
-		head := removeNthFromEnd(dataSet.list, dataSet.target)
+		head := removeNthFromEnd(data.list, data.n)
 
-		for _, val := range dataSet.answer {
+		for _, val := range data.answer {
 			assert.Equal(t, val, head.Val)
 			head = head.Next
 		}
@@ -30,6 +39,7 @@ func getTestDataSet() []TestCase {
 	return []TestCase{
 		{[]int{}, 1, &ListNode{Val: 1}},
 		{[]int{1}, 1, &ListNode{Val: 1, Next: &ListNode{Val: 2}}},
+		{[]int{2}, 2, &ListNode{Val: 1, Next: &ListNode{Val: 2}}},
 		{[]int{1, 2, 3, 5}, 2, &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}},
 	}
 }
